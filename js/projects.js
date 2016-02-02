ae29581cc6a33ea0c8ba0c4828f3fa984f30a0da
Sam Gruse <sgruse89@gmail.com>
2016-02-01 21:34:06 -0800

    Main page layout with generated HTML form object constructor

---
+++ js/projects.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var gitProjects = [
+  {
+    projectName: 'Mic Check',
+    projectUrl: 'http://bgarnaat.github.io/starving_artists/index.html',
+    publishedOn: '2016-1-28',
+    projectImg: 'img/screen1.png',
+    details: '<p>Cupidatat pancetta chuck fugiat shankle dolor drumstick excepteur spare ribs duis. Meatloaf beef ribs et picanha eu in qui dolore tongue enim spare ribs capicola est pork chop swine. Ipsum strip steak nostrud ham hock qui irure. Picanha occaecat prosciutto, rump tempor jerky proident voluptate short loin jowl.</p>'
+  },
+
+  {
+    projectName: 'Buss Mall',
+    projectUrl: 'http://sgruse.github.io/bussMall',
+    publishedOn: '2016-1-23',
+    projectImg: 'img/screen2.png',
+    details: '<p>Cupidatat pancetta chuck fugiat shankle dolor drumstick excepteur spare ribs duis. Meatloaf beef ribs et picanha eu in qui dolore tongue enim spare ribs capicola est pork chop swine. Ipsum strip steak nostrud ham hock qui irure. Picanha occaecat prosciutto, rump tempor jerky proident voluptate short loin jowl.</p>'
+  },
+];
---
+++ readme.md
@@ -0,0 +1,2 @@
+
+This is my portfolio in which I will represent myself to my future employers.  It will contain links my chosen social medias as well as my github page to show off my current and past projects.  
