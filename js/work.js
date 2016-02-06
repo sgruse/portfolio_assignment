a54db1e400cd0de317e55374bf02573a36aa179c
Sam Gruse <sgruse89@gmail.com>
2016-02-05 17:48:56 -0800

    Finished the pair programming review of my project

--- js/work.js
+++ js/work.js
@@ -4,29 +4,12 @@ var objectControllers = {};
 
 objectControllers.handleMainNav = function(event) {
   $('#nav-menu li').on('click', 'a', function(){
-    console.log($(this).data('category'));
+    console.log($(this));
     $('section').hide();
     $('.' + $(this).data('category')).parent().show();
+    // console.log($('.' + $(this).data('category')).parent());
     $('.' + $(this).data('category')).show();
   });
-//     var $main = $('main > section');
-//     var $navClick = $(this).data('category');
-//     // console.log($navClick);
-//     $main.hide();
-//     $main.find('section').hide();
-//     $main.each(function(){
-//       if($(this).attr('class') === $navClick){
-//         $(this).show();
-//       }
-//
-//     });
-//     $main.find('section').each(function(){
-//       if($(this).attr('class') === $navClick){
-//         $(this).show();
-//       }
-//     });
-//     console.log($main.find('section'));
-//   });
 };
 
 $(document).ready(function(){
@@ -34,7 +17,3 @@ $(document).ready(function(){
   $('.introduction').hide();
   objectControllers.handleMainNav();
 });
-
-// $('#nav-menu li').on('click', 'a', function(){
-//   $('#introduction').show();
-// })
