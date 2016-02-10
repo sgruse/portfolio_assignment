9e6d0c81dd5a573731066868e367ab3322995eaf
Sam Gruse <sgruse89@gmail.com>
2016-02-09 19:00:05 -0800

    Updated my media query and also implemented JSON.  Got rid of global variables

--- js/app.js
+++ js/app.js
@@ -1,57 +1,70 @@
 'use strict';
 
-// var projects = [];
+(function(module){
 
-function Project(opts){
-  this.projectName = opts.projectName;
-  this.projectUrl = opts.projectUrl;
-  this.publishedOn = opts.publishedOn;
-  this.projectImg = opts.projectImg;
-  this.details = opts.details;
-};
+  function Project(opts){
+    this.projectName = opts.projectName;
+    this.projectUrl = opts.projectUrl;
+    this.publishedOn = opts.publishedOn;
+    this.projectImg = opts.projectImg;
+    this.details = opts.details;
+  };
 
-Project.all = [];
+  Project.all = [];
 
+  Project.prototype.toHtml = function(){
+    var source = $('#entry-template').text();
+    var template = Handlebars.compile(source);
+    console.log('cat');
 
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-Project.prototype.toHtml = function(){
-  var source = $('#entry-template').text();
-  var template = Handlebars.compile(source);
+    return template(this);
+  };
 
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  Project.loadAll = function(gitProjects){
 
-  return template(this);
-};
-
-Project.loadAll = function(gitProjects){
-  gitProjects.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
-
-  gitProjects.forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-};
-
-Project.fetchAll = function(){
-  if(localStorage.gitProjects){
-    Project.loadAll(JSON.parse(localStorage.gitProjects));
-    objectControllers.handleMainNav();
-  } else {
-    $.ajax({
-      type: 'GET',
-      url: 'data/projects.json',
-      success: function(data, message, xhr) {
-        localStorage.setItem('gitProjects', JSON.stringify(data));
-        Project.loadAll(JSON.parse(localStorage.gitProjects));
-        objectControllers.handleMainNav();
-      }
+    gitProjects.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
     });
-  }
-};
 
+    gitProjects.forEach(function(ele) {
+      Project.all.push(new Project(ele));
+    });
+  };
 
-// projects.forEach(function(a){
-//   $('#projectSection').append(a.toHtml());
-// });
+  Project.fetchAll = function(){
+    // console.log('fetchAll');
+    if(localStorage.gitProjects){
+      $.ajax({
+        type: 'HEAD',
+        url: 'data/projects.json',
+        success: function(data, message, xhr){
+          var eTag = xhr.getResponseHeader('eTag');
+          if (!localStorage.eTag || eTag !== localStorage.eTag) {
+            localStorage.eTag = eTag;
+            Project.getData();
+          } else {
+            Project.loadAll(JSON.parse(localStorage.gitProjects));
+            objectControllers.initIndexPage();
+          }
+        }
+      });
+
+    } else {
+      Project.getData();
+      objectControllers.initIndexPage();
+    }
+  };
+
+  Project.getData = function(){
+    $.getJSON('data/projects.json', function(gitProjects){
+      Project.loadAll(gitProjects);
+      localStorage.gitProjects = JSON.stringify(gitProjects);
+      objectControllers.handleMainNav();
+    });
+  };
+
+  module.Project = Project;
+})(window);
