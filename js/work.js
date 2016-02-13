e1df9fd821d3fa22fc2d8fddbb8cb90bbeb7411a
Sam Gruse <sgruse89@gmail.com>
2016-02-13 14:21:02 -0800

    made more styling changes and worked through issues from Kat

--- js/work.js
+++ js/work.js
@@ -1,21 +1,26 @@
 'use strict';
 
-var objectControllers = {};
+(function(module){
 
-objectControllers.handleMainNav = function(event) {
-  $('#nav-menu li').on('click', 'a', function(){
-    console.log($(this));
-    $('section').hide();
-    $('.' + $(this).data('category')).parent().show();
-    $('.' + $(this).data('category')).fadeIn('slow');
-  });
-};
+  var objectControllers = {};
 
-objectControllers.initIndexPage = function(){
-  Project.all.forEach(function(a){
-    $('#projectSection').append(a.toHtml());
-  });
-  $('#projectSection').hide();
-  $('.introduction').hide();
-  objectControllers.handleMainNav();
-};
+  objectControllers.handleMainNav = function(event) {
+    $('#nav-menu li').on('click', 'a', function(){
+      console.log($(this));
+      $('section').hide();
+      $('.' + $(this).data('category')).parent().show();
+      $('.' + $(this).data('category')).fadeIn('slow');
+    });
+  };
+
+  objectControllers.initIndexPage = function(){
+    Project.all.forEach(function(a){
+      $('#projectSection').append(a.toHtml());
+    });
+    $('#projectSection').hide();
+    $('.introduction').hide();
+    objectControllers.handleMainNav();
+  };
+
+  module.objectControllers = objectControllers;
+})(window);
